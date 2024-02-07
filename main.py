7f721cda61630cfa385885a802a3bad53bf6ccea
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-02-07 18:00:20 +0200

    initial: setup project

---
+++ main.py
@@ -0,0 +1,9 @@
+def raft(courses: int, goats: int, weights: list[int]):
+    pass
+
+
+if __name__ == '__main__':
+    courses = 2
+    goats = 6
+    weights = [26 ,7, 10, 30, 5, 4]
+    raft(courses, goats, weights)
